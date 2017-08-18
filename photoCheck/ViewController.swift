901df88c937b1be7afbf6f8e58201c5af1bcfcc5
MEI KU <natalie@MEIde-MacBook-Air.local>
2017-08-18 23:37:01 +0800

    Initial Commit

---
+++ photoCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoCheck
+//
+//  Created by MEI KU on 2017/8/18.
+//  Copyright © 2017年 Natalie KU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
